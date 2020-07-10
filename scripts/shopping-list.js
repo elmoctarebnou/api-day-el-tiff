4fbc17ada5fc4e5900ce2048dd49c9f0996a6b1d
Terra <terra.taylor@gmail.com>
2020-07-10 09:16:51 -0600

    with modules

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,7 +1,5 @@
-const store = {
-  items: [],
-  hideCheckedItems: false
-};
+import store from './store.js';
+import item from './item.js';
 
 const generateItemElement = function (item) {
   let itemTitle = `<span class="shopping-item shopping-item__checked">${item.name}</span>`;
@@ -38,14 +36,12 @@ const render = function () {
   if (store.hideCheckedItems) {
     items = items.filter(item => !item.checked);
   }
+
   // render the shopping list in the DOM
   const shoppingListItemsString = generateShoppingItemsString(items);
+
   // insert that HTML into the DOM
   $('.js-shopping-list').html(shoppingListItemsString);
-};
-
-const addItemToShoppingList = function (itemName) {
-  store.items.push({ id: cuid(), name: itemName, checked: false });
 };
 
 const handleNewItemSubmit = function () {
@@ -53,20 +49,15 @@ const handleNewItemSubmit = function () {
     event.preventDefault();
     const newItemName = $('.js-shopping-list-entry').val();
     $('.js-shopping-list-entry').val('');
-    addItemToShoppingList(newItemName);
+    store.addItem(newItemName);
     render();
   });
-};
-
-const toggleCheckedForListItem = function (id) {
-  const foundItem = store.items.find(item => item.id === id);
-  foundItem.checked = !foundItem.checked;
 };
 
 const handleItemCheckClicked = function () {
   $('.js-shopping-list').on('click', '.js-item-toggle', event => {
     const id = getItemIdFromElement(event.currentTarget);
-    toggleCheckedForListItem(id);
+    store.findAndToggleChecked(id);
     render();
   });
 };
@@ -77,46 +68,14 @@ const getItemIdFromElement = function (item) {
     .data('item-id');
 };
 
-/**
- * Responsible for deleting a list item.
- * @param {string} id 
- */
-const deleteListItem = function (id) {
-  const index = store.items.findIndex(item => item.id === id);
-  store.items.splice(index, 1);
-};
-
 const handleDeleteItemClicked = function () {
   // like in `handleItemCheckClicked`, we use event delegation
   $('.js-shopping-list').on('click', '.js-item-delete', event => {
     // get the index of the item in store.items
     const id = getItemIdFromElement(event.currentTarget);
     // delete the item
-    deleteListItem(id);
+    store.findAndDelete(id);
     // render the updated shopping list
-    render();
-  });
-};
-
-const editListItemName = function (id, itemName) {
-  const item = store.items.find(item => item.id === id);
-  item.name = itemName;
-};
-
-/**
- * Toggles the store.hideCheckedItems property
- */
-const toggleCheckedItemsFilter = function () {
-  store.hideCheckedItems = !store.hideCheckedItems;
-};
-
-/**
- * Places an event listener on the checkbox
- * for hiding completed items.
- */
-const handleToggleFilterClick = function () {
-  $('.js-filter-checked').click(() => {
-    toggleCheckedItemsFilter();
     render();
   });
 };
@@ -126,7 +85,14 @@ const handleEditShoppingItemSubmit = function () {
     event.preventDefault();
     const id = getItemIdFromElement(event.currentTarget);
     const itemName = $(event.currentTarget).find('.shopping-item').val();
-    editListItemName(id, itemName);
+    store.findAndUpdateName(id, itemName);
+    render();
+  });
+};
+
+const handleToggleFilterClick = function () {
+  $('.js-filter-checked').click(() => {
+    store.toggleCheckedFilter();
     render();
   });
 };
